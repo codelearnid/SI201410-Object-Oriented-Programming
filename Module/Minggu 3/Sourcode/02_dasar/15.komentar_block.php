1f37ce168a7cbc255785d4c42d1d3533c1521701
codelearn <codelearnofficial@gmail.com>
2023-08-23 18:51:52 +0800

    Update materi minggu 6 & 7

---
+++ Module/Minggu 3/Sourcode/02_dasar/15.komentar_block.php
@@ -0,0 +1,8 @@
+<?php
+  /////////////////////////////
+  // Validasi Form Register
+  /////////////////////////////
+
+  // $nama = "Arif Wicaksono Septyanto";
+  $nama = "naura laravella";
+  echo $nama; 
